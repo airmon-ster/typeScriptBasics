a01f09080f6fd7596922431bf7ac8bb1aeec331d
airmonster <airmonster@protonmail.com>
2020-06-25 13:01:42 -0400

    update

---
+++ features/annotations/variables.ts
@@ -0,0 +1,24 @@
+let apples: number = 5
+let speed: string = 'fast'
+let hasName: boolean = true
+let nothing: null = null
+let undef: undefined = undefined
+
+// built in
+
+let now: Date = new Date()
+
+let colors: string[] = ['red', 'blue', 'white']
+let numbers: number[] = [1,2,3]
+let bools: boolean[] = [true, false]
+
+class Car {
+
+}
+let car: Car = new Car()
+
+let point: { x: number; y: number } = {
+    x: 10,
+    y: 20
+}
+
